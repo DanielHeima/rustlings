7c51df7610d4c96c2252117f4414c559f1b28698
DanielHeima <danielhelgi1@gmail.com>
2024-01-20 12:36:26 +0000

    checkpoint

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -39,6 +38,40 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        scores.entry(team_1_name).and_modify(|t1| {
+            t1.goals_scored += &team_1_score;
+            t1.goals_conceded += &team_2_score;
+        }).or_insert(Team {goals_scored: team_1_score, goals_conceded: team_2_score});
+
+        scores.entry(team_2_name).and_modify(|t2| {
+            t2.goals_scored += &team_2_score;
+            t2.goals_conceded += &team_1_score;
+        }).or_insert(Team {goals_scored: team_2_score, goals_conceded: team_1_score});
+
+        // if !scores.contains_key(&team_1_name) {
+        //     scores.insert(team_1_name.clone(), Team { goals_conceded: 0, goals_scored: 0 });
+        // }
+
+        // if !scores.contains_key(&team_2_name) {
+        //     scores.insert(team_2_name.clone(), Team { goals_conceded: 0, goals_scored: 0 });
+        // }
+
+        // match scores.get_mut(&team_1_name) {
+        //     None => (),
+        //     Some(team) => {
+        //         team.goals_scored += &team_1_score;
+        //         team.goals_conceded += &team_2_score;
+        //     }
+        // }
+
+        // match scores.get_mut(&team_2_name) {
+        //     None => (),
+        //     Some(team) => {
+        //         team.goals_scored += &team_2_score;
+        //         team.goals_conceded += &team_1_score;
+        //     }
+        // }
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -19,8 +19,7 @@
 // - The output element is going to be a Vector of strings.
 //
 // No hints this time!
-
-// I AM NOT DONE
+ 
 
 pub enum Command {
     Uppercase,
@@ -32,12 +31,28 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(&str, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
-        }
+        let mut output: Vec<String> = vec![]; 
+        for (string , command) in input {
+            output.push(match command {
+                Command::Uppercase => {
+                    string.to_uppercase()
+                },
+                Command::Trim => {
+                    string.trim().to_string()
+                },
+                Command::Append(u) => {
+                    let mut appendage = "".to_owned();
+                    let mut string_with_appendage: String = string.to_owned();
+                    for _ in 0..u {
+                        appendage.push_str("bar");
+                    };
+                    string_with_appendage.push_str(&appendage);
+                    string_with_appendage
+                }
+            })
+        };
         output
     }
 }
@@ -45,7 +60,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
